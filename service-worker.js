47f6c7d6bcb996b0bb88be0ed3c51efbb9fe5f4c
maochenyun230-byte <maochenyun230@gmail.com>
2026-09-19 18:29:34 +0100

    Fix yuna ovo icon and daily brief mobile layout

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE='yuna-ovo-v1';
-const ASSETS=['./','./index.html','./manifest.webmanifest','./icon.svg'];
+const CACHE='yuna-ovo-v2';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./yuna-ovo-icon-v2.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{
