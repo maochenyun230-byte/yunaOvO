aaa0e55cb884cdc266c57e47595629fd28f35794
maochenyun230-byte <maochenyun230@gmail.com>
2026-09-19 20:08:16 +0100

    Route OvO calls to saved ChatGPT voice conversation

--- v3/service-worker.js
+++ v3/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='yuna-ovo-v4';
+const CACHE='yuna-ovo-v5';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./yuna-ovo-icon-v3.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
