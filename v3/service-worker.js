0eedff5791859eb5391cff10af4b03902197e20a
maochenyun230-byte <maochenyun230@gmail.com>
2026-09-19 20:34:21 +0100

    Add daily rotating inspiration feed and word of the day

--- v3/service-worker.js
+++ v3/service-worker.js
@@ -1,5 +1,5 @@
-const CACHE='yuna-ovo-v6';
-const ASSETS=['./','./index.html','./manifest.webmanifest','./yuna-ovo-icon-v3.png'];
+const CACHE='yuna-ovo-v7';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./yuna-ovo-icon-v3.png','./daily-inspiration.json'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request,{cache:'no-store'}).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r;}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))));});
